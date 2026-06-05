f6127344ede044d3d952fbff9d7acddfae1f15fc
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:59 -0500

    xml commenting

--- src/Soenneker.Vercel.OpenApiClientUtil/Abstract/IVercelOpenApiClientUtil.cs
+++ src/Soenneker.Vercel.OpenApiClientUtil/Abstract/IVercelOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Vercel.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IVercelOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<VercelOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
